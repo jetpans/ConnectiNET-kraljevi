e4ff051a41a560898ecec777a7298758bc1bd2d7
Luka Milicevic <lukalmx@gmail.com>
2023-11-13 21:30:57 +0100

    Patch #4 for deployment - Add express frontend serving

--- frontend/src/config.js
+++
@@ -1,9 +0,0 @@
-let API_URL = '';
-
-if (process.env.NODE_ENV === 'production') {
-  API_URL = require('./config.prod').default;
-} else {
-  API_URL = require('./config.dev').default;
-}
-
-export default API_URL;
--- frontend/src/config.prod.js
+++
@@ -1,3 +0,0 @@
-const API_URL = 'https://progi-be-4bkw.onrender.com';
-
-export default API_URL;

6f5958df3606d3d3c84f72d38d4cdc84aa4d2caf
Luka Milicevic <lukalmx@gmail.com>
2024-01-19 20:58:39 +0100

    patch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,13 @@ import LoginPage from "./views/LoginPage";
 import LandingPage from "./views/LandingPage";
 import EventsPage from "./views/EventsPage";
 import RegisterPage from "./views/RegisterPage";
-import SubscribePage from "./views/SubscribePage";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/events" element={<EventsPage />} />
-        <Route path="/subscribe" element={<SubscribePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="*" element={<h1>404</h1>} />
